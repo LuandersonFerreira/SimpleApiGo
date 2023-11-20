a594d9f1d41f17dd89dca0863eb37d99e837c192
Luanderson Ferreira Cavalcante <luanderson.cavalcante@unico.io>
2023-11-20 18:07:40 -0300

    feat: :sparkles: my first commit

---
+++ db/connection.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"apiGo/configs"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection() (*sql.DB, error) {
+	conf := configs.GetDB()
+
+	stringConnection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
+
+	conn, err := sql.Open("postgres", stringConnection)
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
